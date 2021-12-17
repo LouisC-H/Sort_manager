32dd4f67070f0779cd0fde0a2762c15a258e2303
Louis C-H <louisclementharris@yahoo.co.uk>
2021-12-17 14:38:24 +0000

    Wide-ranging code refactor, including adding significantly more tests and instances of logging. Found a new bug which I can't solve which is described in comments in the app class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,19 +14,9 @@ public class App
     repeating yourself, there's always an alternative solution to make things even more streamlined
     solution.
 
-    KNOWN BUG: when if a binary tree sort is carried out AFTER a BubbleSort (3) or DefaultSort (4), and the list size is relatively large
-    (5000 is about the largest you can safely go), the code throws up a StackOverflow error. Normally the tree sort can handle much, much larger
-    numbers (Note: using logging, we can see that the tree sorter doesn't seem to get stuck or anything??)
-
     extensions:
-
-    go back over and check to add more tests
-    extensive logging (multiple file outputs?)
-    set up code so that it can very easily be extended with new sorting algorithms
     use generics to ask them what number type they want to use
     Graphing =)
-
-    More confidence, clean code
      */
     public static void main( String[] args )
     {
